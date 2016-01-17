dcc29606b3f8328fdcc8afec449d61749fe0b849
bingcai <bingcai@bingcais-MacBook-Pro.local>
2016-01-16 21:04:02 -0800

    add first

---
+++ BPNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BPNews
+//
+//  Created by bingcai on 16/1/16.
+//  Copyright © 2016年 bingcai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
